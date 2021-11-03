a71f301e86728ab8f161ffcd12599e0818569c32
Peter Oliveira <stack.peter21@gmail.com>
2021-11-03 19:21:18 -0300

    small changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./Components/Header";
+import Header from "../src/Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
